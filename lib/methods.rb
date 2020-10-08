2e73afddbe443fb19317bb2d780687b089bb2f45
Abhigyan <abhigyanmahanta001@gmail.com>
2020-10-08 18:49:00 +0530

    create method for user input and verifying it

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,2 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+
+class Scrapeit
+  def initialize; end
+
+  def check_input(input)
+    if input < 31 && input >= 1
+      true
+    else
+      false
+    end
+  end
+end
